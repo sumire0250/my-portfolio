94e1c6cd01a336adea15235b797d1d6ebff5a63c
sumire0250 <sumire0250@gmail.com>
2026-08-24 03:00:19 +0900

    Add files via upload

---
+++ js/slideshow.js
@@ -0,0 +1,27 @@
+const images = [
+    "../images/my_workout.png",
+    "../images/IMG_E0636.jpg"
+];
+
+let current = 0;
+
+const image = document.getElementById("work-image");
+
+setInterval(() => {
+
+    // 現在の画像を消す
+    image.style.opacity = 0;
+
+    // 0.8秒後に画像を変更
+    setTimeout(() => {
+
+        current = (current + 1) % images.length;
+
+        image.src = images[current];
+
+        // 新しい画像を表示
+        image.style.opacity = 1;
+
+    }, 800);
+
+}, 3000);
